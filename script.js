64bf693cd6f3fba4f72ae86d793a778889cdf00c
manon <manon.avaullee@epitech.eu>
2024-01-10 16:48:39 +0100

    ADD - Amélioration de l'interface graphique

--- script.js
+++ script.js
@@ -8,89 +8,60 @@ let listeDeroulante_type = document.getElementById("option_recherche");
 
 let listeAPuces = document.getElementById("listeAPuces");
 
-let lstCourant = [];
-let lstNote = [];
-let lstEmail = [];
-let lstTodo = [];
 
-let listeChoisie = []; // Stockez les chaînes de caractères ici
-let elementsChoisis = []; // Stockez les éléments HTML correspondants ici
 
-function ajouteUnElementDansLaListe(input_text, listeDeroulante) {
-    let itemSelectionne = listeDeroulante.value;
-    console.log('l item selectionne est : ' + itemSelectionne);
+let tousElementsLi = document.querySelectorAll('li');
+let typeSelectionne = listeDeroulante.value;
 
-    const newLi = document.createElement('li');
-    newLi.classList.add(itemSelectionne);
-    newLi.textContent = input_text.value;
 
-    document.getElementById('listeAPuces').appendChild(newLi);
+let tabNotes = [
+    {texte:"C'est la première note", type: "note"},
+];
 
-    lstCourant.push(input_text.value);
+let tabNotesFiltre = [
+    {texte:"C'est la première note filtrée", type: "note"},
+];
 
-    // Stockez la chaîne de caractères et l'élément HTML correspondant
-    listeChoisie.push(input_text.value);
-   
 
-    if (itemSelectionne == "note") {
-        lstNote.push(input_text.value);
-    } else {
-        if (itemSelectionne == "email") {
-            lstEmail.push(input_text.value);
-        } else {
-            lstTodo.push(input_text.value);
-        }
-    }
-}
 
-document.addEventListener('DOMContentLoaded', function () {
-    btnValide.addEventListener('click', function (event) {
-        event.preventDefault();
-        ajouteUnElementDansLaListe(input_text, listeDeroulante);
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-
-    btnRecherche.addEventListener('click', function (event) {
-
-        event.preventDefault();
-
-        let typeSelectionne = listeDeroulante_type.value;
-
-        while (listeAPuces.firstChild) {
-            listeAPuces.removeChild(listeAPuces.firstChild);
-        }
-
-        console.log("la liste est vidée");
-
-        if (typeSelectionne == "note") {
-            listeChoisie = lstNote;
-            console.log("la liste est 1");
-        } else {
-            if (typeSelectionne == "email") {
-                listeChoisie = lstEmail;
-                console.log("la liste est 2");
-            } else {
-                listeChoisie = lstTodo;
-                console.log("la liste est 3");
-            }
-        }
-
-        elementsChoisis = listeChoisie;
-        // Affichez les éléments HTML stockés dans elementsChoisis
-        elementsChoisis.forEach(function (element) {
-            document.getElementById('listeAPuces').appendChild(element);
-        });
-
-    });
-});
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
 });
 
+//Ajouter une ligne dans la liste 
+document.addEventListener('DOMContentLoaded', function () {
+    btnValide.addEventListener('click', function (event) {
+        event.preventDefault();
+        ajouteUnElementDansLaListe(input_text.value, listeDeroulante.value);
+    });
+});
+
+function ajouteUnElementDansLaListe(texte, type) {
+ 
+    //Ajout de l'élément dans le tableau
+    tabNotes.push({texte: "- " + texte, type: type});
+
+    //Vider la liste
+    while (listeAPuces.firstChild) {
+        listeAPuces.removeChild(listeAPuces.firstChild);
+    }
+
+    for(var i = 0; i<tabNotes.length;i++)
+    {
+        var newLi = document.createElement("li");
+        newLi.textContent = tabNotes[i].texte;
+        newLi.classList.add(tabNotes[i].type);
+
+        listeAPuces.appendChild(newLi);
+    }
+
+    //console.log(tabNotes);
+
+}
+
+//Supression d'un élément de la liste
 document.addEventListener('DOMContentLoaded', function () {
     var maListe = document.getElementById('listeAPuces');
 
@@ -107,4 +78,58 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+//Filtrage des éléments de la liste 
+document.addEventListener('DOMContentLoaded', function () {
 
+    btnRecherche.addEventListener('click', function (event) {
+        event.preventDefault();   
+
+        let typeRecherche = listeDeroulante_type.value;
+        
+        tabNotesFiltre = [];
+
+        // Vider la liste
+        while (listeAPuces.firstChild) {
+            listeAPuces.removeChild(listeAPuces.firstChild);
+        }
+
+        console.log("apres vidage " + listeAPuces);
+
+        for(var i = 0; i<tabNotes.length;i++)
+        {
+            console.log("type de la recherche : " + typeRecherche);
+            console.log("type note : " + tabNotes[i].type);
+           
+
+            if(tabNotes[i].type === typeRecherche)
+            {
+                tabNotesFiltre.push(tabNotes[i]);    
+                console.log(tabNotesFiltre);
+                
+            }
+        }
+
+        for(var i = 0; i<tabNotesFiltre.length;i++)
+        {
+            var newLi = document.createElement("li");
+            newLi.textContent = tabNotesFiltre[i].texte;
+            newLi.classList.add(tabNotesFiltre[i].type);
+
+            listeAPuces.appendChild(newLi);
+        }
+        
+
+    });
+
+});
+//Redimensionnement de la fenêtre
+
+window.addEventListener('resize', function() {
+    
+    var largeurMinimale = 365; 
+    
+    if (window.innerWidth < largeurMinimale) {
+        
+        window.resizeTo(largeurMinimale, window.innerHeight);
+    }
+});
